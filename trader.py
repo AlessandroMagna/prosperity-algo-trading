533e109a2e44bc3ec2d96bef7e31530cbf293a8b
Alessandro Magnabosco <113511792+AlessandroMagna@users.noreply.github.com>
2024-03-29 17:09:11 +0100

    Update trader.py

--- trader.py
+++ trader.py
@@ -5,10 +5,15 @@ import string
 class Trader:
     
     def run(self, state: TradingState):
-        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
+        """
+        
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
+        
         result = {}
+        
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
